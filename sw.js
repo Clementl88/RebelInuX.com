d4c97a6de467c4447791a821b308ccdbe3e0ee34
Clementl88 <61205692+Clementl88@users.noreply.github.com>
2025-12-24 15:01:32 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js - NETWORK FIRST strategy
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
